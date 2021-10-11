c1e348425ad4eac21dab352235194df86606f6c2
max-sliva <persimon@inbox.ru>
2021-10-11 14:15:53 +0500

    update

--- src/Java/PortScanner.java
+++ src/Java/PortScanner.java
@@ -1,8 +1,10 @@
 package Java;
 
 import jssc.SerialPort;
+import jssc.SerialPortList;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class PortScanner {
@@ -21,6 +23,23 @@ public class PortScanner {
 
     public SerialPort getSerialPortByArdu(String str) {
         return portArdus.get(str);
+    }
+
+    public void printPortsArray() { //вывод списка портов
+        System.out.println("Ports changed");
+        if (portNames.size() == 0) System.out.println("No arduinos");
+        else portNames.forEach ( it ->
+            System.out.println(it)
+        );
+    }
+
+    public void startUSBscanner() {
+        System.out.println("Start thread for scanning ports");
+        portNames = (ArrayList<String>) Arrays.asList(SerialPortList.getPortNames()); // получаем список портов
+        portsHashMap = new HashMap<String, SerialPort>();
+        String[] portNames2 = SerialPortList.getPortNames(); // получаем список портов, с ним будем потом сравнивать новый список
+        printPortsArray();
+
     }
 
 }
